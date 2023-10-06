8c8a97d78d381e0f3af90f547cc24b88eb558d53
mushid <munshid.ds@gmail.com>
2023-10-06 12:05:37 +0530

    added token

--- app.py
+++ app.py
@@ -1,21 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import joblib
-import sklearn
-
+from flask import Flask, render_template, request, jsonify
+import joblib
+import secrets
 
 app = Flask(__name__)
 
 # Load the pre-trained machine learning model
 model = joblib.load("iris_model.pkl")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Secret key for generating and verifying tokens
+SECRET_KEY = "ab_123"
+
+# Generate a token for authentication
+def generate_token():
+    return secrets.token_hex(16)
+
+# Authenticate users with a token
+def authenticate_token(token):
+    return token == SECRET_KEY
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get user input from the form
+        # Get the token from the request headers
+        token = request.headers.get('Authorization')
+
+        # Authenticate the token against your secret key
+        if not authenticate_token(token):
+            return jsonify({'error': 'Authentication failed'}), 401
+
+        # Get the input data from the form
         sepal_length = float(request.form['sepal_length'])
         sepal_width = float(request.form['sepal_width'])
         petal_length = float(request.form['petal_length'])
@@ -28,9 +44,11 @@ def predict():
         classes = ['Setosa', 'Versicolor', 'Virginica']
         predicted_class = classes[int(prediction)]
 
-        return render_template('index.html', prediction=predicted_class)
+        return jsonify({'prediction': predicted_class})
     except Exception as e:
-        return render_template('index.html', error=str(e))
+        return jsonify({'error': str(e)})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
